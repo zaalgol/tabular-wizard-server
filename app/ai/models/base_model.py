7f16fe7447866744c565b91961e8dfdbc1d1037c
zaalgol <zaalgol@gmail.com>
2024-05-17 10:25:41 +0300

    removing debugging, parralleism, refactoring base_model

--- app/ai/models/base_model.py
+++ app/ai/models/base_model.py
@@ -13,33 +13,41 @@ class BaseModel:
     def __init__(self, train_df, target_column, scoring, split_column=None,
                  create_encoding_rules=False, apply_encoding_rules=False, create_transformations=False, apply_transformations=False, test_size=0.25,
                  already_splitted_data=None):
+        self.train_df = train_df
         self.search = None
         self.scoring = scoring
         self.target_column = target_column
         self.data_preprocessing = DataPreprocessing()
         self.encoding_rules = None
-
+        
+        self.__split_data(split_column,
+                 create_encoding_rules, apply_encoding_rules, create_transformations, apply_transformations, test_size,
+                 already_splitted_data)
+        
+    def __split_data(self, split_column,
+                 create_encoding_rules, apply_encoding_rules, create_transformations, apply_transformations, test_size,
+                 already_splitted_data):
         if already_splitted_data:
             self.X_train, self.X_test, self.y_train, self.y_test = \
                 already_splitted_data['X_train'], already_splitted_data['X_test'], already_splitted_data['y_train'], already_splitted_data['y_test']
             return
         
         if split_column is None:
-            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(train_df,
-                                                                                        train_df[target_column],
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split( self.train_df,
+                                                                                        self.train_df[self.target_column],
                                                                                         test_size=test_size, random_state=42)
         else:
             splitter = GroupShuffleSplit(test_size=test_size, n_splits=2, random_state=7)
-            split = splitter.split(train_df, groups=train_df[split_column])
+            split = splitter.split( self.train_df, groups= self.train_df[split_column])
             train_inds, test_inds = next(split)
 
-            train = train_df.iloc[train_inds]
-            self.y_train = train[[target_column]].astype(float)
-            test = train_df.iloc[test_inds]
-            self.y_test = test[[target_column]].astype(float)
+            train =  self.train_df.iloc[train_inds]
+            self.y_train = train[[self.target_column]].astype(float)
+            test =  self.train_df.iloc[test_inds]
+            self.y_test = test[[self.target_column]].astype(float)
 
-        self.X_train = self.X_train.drop([target_column], axis=1)
-        self.X_test = self.X_test.drop([target_column], axis=1)
+        self.X_train = self.X_train.drop([self.target_column], axis=1)
+        self.X_test = self.X_test.drop([self.target_column], axis=1)
         
         self._preprocess_data(create_encoding_rules, apply_encoding_rules, create_transformations, apply_transformations)
         self.X_train = self.data_preprocessing.convert_datatimes_columns_to_normalized_floats(self.X_train)
--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from app.app import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0',port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0',port=8080)
     
 # uwsgi --http :8080 --gevent 1000 --enable-threads --lazy-apps --http-websockets --master --wsgi-file run.py --callable app
